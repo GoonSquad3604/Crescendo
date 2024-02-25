220144a0dbc3fbf7724a0aa8219bfd818c8a5ab6
GBreezys <156611790+GBreezys@users.noreply.github.com>
2024-02-24 20:17:56 -0500

    abot

--- 2024ABot/src/main/java/frc/robot/BuildConstants.java
+++ 2024ABot/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024ABot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 57;
-  public static final String GIT_SHA = "5a56acfead4dcd4843fa499787a334c208b475e4";
-  public static final String GIT_DATE = "2024-02-24 19:06:55 EST";
+  public static final int GIT_REVISION = 58;
+  public static final String GIT_SHA = "9b7f18311a4aa1fdadeaabf6bd6d9b5f025caba7";
+  public static final String GIT_DATE = "2024-02-24 20:05:17 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-02-24 19:57:55 EST";
-  public static final long BUILD_UNIX_TIME = 1708822675311L;
+  public static final String BUILD_DATE = "2024-02-24 20:16:05 EST";
+  public static final long BUILD_UNIX_TIME = 1708823765029L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
