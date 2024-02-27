cfc10d139d56fae9c2b1a49299839c96fa44d843
GBreezys <156611790+GBreezys@users.noreply.github.com>
2024-02-26 20:37:14 -0500

    new generated swerve

--- 2024ABot/src/main/java/frc/robot/BuildConstants.java
+++ 2024ABot/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024ABot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "15e3a9ea37a59318f2d664c595d7441c11cb03aa";
-  public static final String GIT_DATE = "2024-02-25 14:10:44 EST";
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "8783de8780f717c151c882ff8e5dc14d92e38e0e";
+  public static final String GIT_DATE = "2024-02-26 17:56:25 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-02-25 16:44:11 EST";
-  public static final long BUILD_UNIX_TIME = 1708897451016L;
+  public static final String BUILD_DATE = "2024-02-26 20:31:09 EST";
+  public static final long BUILD_UNIX_TIME = 1708997469375L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
