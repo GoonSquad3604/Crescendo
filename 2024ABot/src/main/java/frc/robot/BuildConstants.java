9c54e9830068e09b7a5ad0635ad915cb4d0d8610
unknown <ahmedvirk@wbsdweb.com>
2024-03-23 08:49:28 -0400

    changed the path a litte bit

--- 2024ABot/src/main/java/frc/robot/BuildConstants.java
+++ 2024ABot/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024ABot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 104;
-  public static final String GIT_SHA = "5a2df70f72dfa89064e9575954a3824e83d25e12";
-  public static final String GIT_DATE = "2024-03-22 21:45:27 EDT";
+  public static final int GIT_REVISION = 105;
+  public static final String GIT_SHA = "092757cb517806fbfbe7b064fd0c83340df3abe6";
+  public static final String GIT_DATE = "2024-03-23 08:31:30 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-23 08:11:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1711195861285L;
+  public static final String BUILD_DATE = "2024-03-23 08:45:55 EDT";
+  public static final long BUILD_UNIX_TIME = 1711197955457L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
