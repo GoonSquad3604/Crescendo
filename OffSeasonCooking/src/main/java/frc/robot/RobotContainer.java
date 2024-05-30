3e75b184e4048b214691559e7447ea4ab6473237
unknown <simonphillips@wbsdweb.com>
2024-05-29 20:27:42 -0400

    intake pid (working)

--- OffSeasonCooking/src/main/java/frc/robot/RobotContainer.java
+++ OffSeasonCooking/src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -23,12 +26,30 @@ public class RobotContainer {
   
   private final Shooter s_Shooter = Shooter.getInstance();
   private final Indexer s_Indexer = Indexer.getInstance();
-private final Intake s_Intake = Intake.getInstance();
+  private final Intake s_Intake = Intake.getInstance();
+
+  private final SendableChooser<Command> autoChooser;
 
 
   public RobotContainer() {
+    autoChooser = AutoBuilder.buildAutoChooser();
+    
+    NamedCommands.registerCommand(
+      "revShooter", new InstantCommand(() -> s_Shooter.runShooter()));
+    NamedCommands.registerCommand(
+      "stopShooter", new InstantCommand(() -> s_Shooter.stopShooter()));
+    NamedCommands.registerCommand(
+      "runIndexer", new InstantCommand(() -> s_Indexer.runIndexer()));
+    NamedCommands.registerCommand(
+      "runIntake", new InstantCommand(() -> s_Intake.runIntake()));
+    NamedCommands.registerCommand(
+      "hingeDown", new InstantCommand(() -> s_Intake.setHingeTo(-0.27)));
+
     m_drivetrain.setDefaultCommand(new DefaultDrive(() -> driver.getLeftY(), () -> driver.getRightX()));
+
     configureBindings();
+
+    SmartDashboard.putData("Auto Mode", autoChooser);
   }
 
   private void configureBindings() {
@@ -49,19 +70,19 @@ private final Intake s_Intake = Intake.getInstance();
     driver.x().onTrue(new InstantCommand(() -> s_Intake.lowerHinge()));
     driver.x().onFalse(new InstantCommand(() -> s_Intake.stopHinge()));
 
-    // driver.rightStick().onTrue(new InstantCommand(() -> s_Intake.runIntake()));
-    // driver.rightStick().onFalse(new InstantCommand(() -> s_Intake.stopIntake()));
+    driver.rightStick().onTrue(new InstantCommand(() -> s_Intake.runIntake()));
+    driver.rightStick().onFalse(new InstantCommand(() -> s_Intake.stopIntake()));
 
-    // driver.rightTrigger().onTrue(new InstantCommand(() -> s_Shooter.runShooter()));
-    // driver.rightTrigger().onFalse(new InstantCommand(() -> s_Shooter.stopShooter()));
+    driver.rightTrigger().onTrue(new InstantCommand(() -> s_Shooter.runShooter()));
+    driver.rightTrigger().onFalse(new InstantCommand(() -> s_Shooter.stopShooter()));
 
-    // driver.leftTrigger().onTrue(new ParallelCommandGroup(new InstantCommand(() -> s_Intake.runIntake()),
-    //   new InstantCommand(() -> s_Indexer.runIndexer())));
-    //    driver.leftTrigger().onFalse(new ParallelCommandGroup(new InstantCommand(() -> s_Intake.stopIntake()),
-    //   new InstantCommand(() -> s_Indexer.stopIndexer())));
+    driver.leftTrigger().onTrue(new ParallelCommandGroup(new InstantCommand(() -> s_Intake.runIntake()),
+      new InstantCommand(() -> s_Indexer.runIndexer())));
+       driver.leftTrigger().onFalse(new ParallelCommandGroup(new InstantCommand(() -> s_Intake.stopIntake()),
+      new InstantCommand(() -> s_Indexer.stopIndexer())));
 
     driver.leftTrigger().onTrue(new ParallelCommandGroup(
-      new InstantCommand(() -> s_Intake.setHingeTo(-0.26)),
+      new InstantCommand(() -> s_Intake.setHingeTo(-0.27)),
         new InstantCommand(() -> s_Indexer.runIndexer()), 
           new InstantCommand(() -> s_Intake.runIntake())));
 
@@ -78,6 +99,6 @@ private final Intake s_Intake = Intake.getInstance();
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
--- OffSeasonCooking/src/main/java/frc/robot/subsystems/Intake.java
+++ OffSeasonCooking/src/main/java/frc/robot/subsystems/Intake.java
@@ -7,8 +7,11 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,27 +19,35 @@ public class Intake extends SubsystemBase {
 
   private static Intake _instance;
   private WPI_TalonSRX bigWheel;
-  private WPI_TalonSRX hinge;
+  private WPI_TalonSRX hingeMotor;
   private WPI_TalonSRX mainIntake;
-  private PIDController hingePID;
-
+  
+  private ElevatorFeedforward hingeFeedForward;
   private DutyCycleEncoder hingeEncoder;
-  private double hingeTop = 0.12;
-  private double hingeBottom = -0.16;
+  private TrapezoidProfile.Constraints m_constraints;
+  private ProfiledPIDController hingePIDController;
+
+  private double lastSpeed;
+  private double lastTime;
+
 
   
   public Intake() {
 
     bigWheel = new WPI_TalonSRX(9);
-    hinge = new WPI_TalonSRX(10);
-    hinge.setNeutralMode(NeutralMode.Brake);
+    hingeMotor = new WPI_TalonSRX(10);
+    hingeMotor.setNeutralMode(NeutralMode.Brake);
     mainIntake = new WPI_TalonSRX(11);
 
     hingeEncoder = new DutyCycleEncoder(0);
     hingeEncoder.reset();
 
-    hingePID = new PIDController(1, 0, 0);
-        
+    m_constraints = new TrapezoidProfile.Constraints(2.75, 0.75);
+
+    hingePIDController = new ProfiledPIDController(100, 0, 0, m_constraints, 0.02);
+
+    hingeFeedForward = new ElevatorFeedforward(1.1, 1.2, 1.3);
+
   }
 
    /** Creates a new Intake. */
@@ -48,10 +59,8 @@ public class Intake extends SubsystemBase {
   }
 
   public void runIntake(){
-     bigWheel.set(0.3);
-     mainIntake.set(-0.3);
-
-    
+     bigWheel.set(0.6);
+     mainIntake.set(-0.6);
   }
 
   public void stopIntake(){
@@ -60,25 +69,36 @@ public class Intake extends SubsystemBase {
   }
 
   public void raiseHinge(){
-    hinge.set(0.3);
+    hingeMotor.set(0.3);
   }
 
   public void lowerHinge(){
-    hinge.set(-0.3);
+    hingeMotor.set(-0.3);
   }
 
   public void stopHinge(){
-    hinge.set(0);
+    hingeMotor.set(0);
   }
 
   public void setHingeTo(double position){
-    hinge.set(hingePID.calculate(hingeEncoder.getDistance(), position));
+    hingePIDController.setGoal(position);
+    double pidVal = hingePIDController.calculate(hingeEncoder.getDistance(), position);
+    double acceleration = (hingePIDController.getSetpoint().velocity - lastSpeed) / (Timer.getFPGATimestamp() - lastTime);
+    hingeMotor.setVoltage(
+      pidVal
+      + hingeFeedForward.calculate(hingePIDController.getSetpoint().velocity, acceleration));
+
+    lastSpeed = hingePIDController.getSetpoint().velocity;
+    lastTime = Timer.getFPGATimestamp();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("encoder pos", hingeEncoder.getDistance());
-    SmartDashboard.putNumber("calculated number", hingePID.calculate(hingeEncoder.getDistance(), -.26));
+
+    hingeMotor.setVoltage(
+        hingePIDController.calculate(hingeEncoder.getDistance())
+            + hingeFeedForward.calculate(hingePIDController.getSetpoint().velocity));
   }
 }
